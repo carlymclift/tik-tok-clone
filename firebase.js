7fdb24e1585ea8bae777e0c1aefb448f81f49212
Carly Clift <carlymclift@gmail.com>
2021-01-12 17:28:07 -0700

    Add firebase config file

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDuIwJdvapbKDUkIEvoRNwkhq-iL1Q7oT8",
+  authDomain: "tik-tok-clone-follow-along.firebaseapp.com",
+  projectId: "tik-tok-clone-follow-along",
+  storageBucket: "tik-tok-clone-follow-along.appspot.com",
+  messagingSenderId: "364918216032",
+  appId: "1:364918216032:web:b2630e90717d73a7bd3a05",
+  measurementId: "G-D5M75VM1SS"
+};
