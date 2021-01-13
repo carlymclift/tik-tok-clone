177916451e36a15f824241f7e79a293a5e03a387
Carly Clift <carlymclift@gmail.com>
2021-01-13 13:12:22 -0700

    Create initial video component basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Video from './Video'
 
 function App() {
   return (
     //BEM - lowercase classnames I believe
     <div className="app">
       <h1>hello, lets build an app</h1>
+      <div className='app__videos '>
+        <Video />
+      </div>
     </div>
   );
 }
---
+++ src/Video.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Video.css'
+
+function Video() {
+    return (
+        <div className='video' >
+            <h1>this is the video component</h1>
+        </div>
+    )
+}
+
+export default Video
