c3415fa703d715acc7fa1d441ea07794a05f803f
Carly Clift <carlymclift@gmail.com>
2021-01-13 20:41:18 -0700

    Initial commit

---
+++ src/VideoFooter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './VideoFooter.css'
+
+function VideoFooter() {
+  return (
+    <div className='video__footer' >
+      <h1>this is the footer</h1>
+    </div>
+  )
+}
+
+export default VideoFooter
