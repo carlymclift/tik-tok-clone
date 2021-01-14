183897400a2494c02e179d90aeff16e6fc2bb508
Carly Clift <carlymclift@gmail.com>
2021-01-13 21:28:52 -0700

    Import and use Ticker for song display

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import Ticker from 'react-ticker'
 import './VideoFooter.css'
 
 function VideoFooter() {
   return (
-    <div className='video__footer' >
-      <h1>this is the footer</h1>
+    <div className='videoFooter' >
+      <div className='videoFooter__text' >
+        <h3>@carlyclift</h3>
+        <p>this is a video description</p>
+        <MusicNoteIcon />
+        <Ticker mode='smooth'>
+          {({ index }) => (
+            <>
+              <h1>Song goes here</h1>
+            </>
+          )}
+        </Ticker>
+      </div>
+      <img className='videoFooter__record' src='https://static.thenounproject.com/png/934821-200.png' alt='' />
     </div>
   )
 }
