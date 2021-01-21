38640681fdd2a0a4d670c864aed2e74945b1a6fc
Carly Clift <carlymclift@gmail.com>
2021-01-20 20:19:20 -0700

    Import icons from material ui

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import './VideoSidebar.css'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import MessageIcon from '@material-ui/icons/Message'
+import ShareIcon from '@material-ui/icons/Share'
 
 function VideoSidebar() {
   return (
-    <div>
+    <div className='videoSidebar' >
       <h3>I am a sidebar</h3>
     </div>
   )
