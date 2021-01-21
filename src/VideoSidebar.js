f62ce380694d6fe3629146cb95918822cd1e0c7d
Carly Clift <carlymclift@gmail.com>
2021-01-20 21:11:59 -0700

    Display likes and allow liking incrementing

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './VideoSidebar.css'
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
 import MessageIcon from '@material-ui/icons/Message'
 import ShareIcon from '@material-ui/icons/Share'
+import { FavoriteBorder } from '@material-ui/icons'
 
-function VideoSidebar() {
+function VideoSidebar({ likes, shares, comments }) {
+  const [liked, setLiked] = useState(false)
+
   return (
     <div className='videoSidebar' >
-      <h3>I am a sidebar</h3>
+      <div className='videoSidebar__button' >
+        {liked ? <FavoriteIcon onClick={(e) => setLiked(false)} /> : <FavoriteBorderIcon onClick={(e) => setLiked(true)}/>}
+        <p>{liked ? likes + 1 : likes}</p>
+      </div>
+      <div className='videoSidebar__button' >
+        <MessageIcon />
+        <p>{comments}</p>
+      </div>
+      <div className='videoSidebar__button' >
+        <ShareIcon />
+        <p>{shares}</p>
+      </div>
     </div>
   )
 }
