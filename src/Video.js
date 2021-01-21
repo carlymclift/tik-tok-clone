d14136a70a8d7f2bdfbd5ebd00db0ff9608186df
Carly Clift <carlymclift@gmail.com>
2021-01-20 19:51:28 -0700

    Add props to video footer component

--- src/Video.js
+++ src/Video.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
 import VideoFooter from './VideoFooter'
+import VideoSidebar from './VideoSidebar'
 import './Video.css'
 
 function Video() {
@@ -26,8 +27,12 @@ function Video() {
           ref={videoRef}
           src="https://v16-web.tiktok.com/video/tos/useast2a/tos-useast2a-pve-0068/7c7e782b840340a4852d64401e68b2e6/?a=1988&br=2508&bt=1254&cd=0%7C0%7C1&cr=0&cs=0&cv=1&dr=0&ds=3&er=&expire=1610592720&l=202101132051310101890740211C2238AA&lr=tiktok_m&mime_type=video_mp4&policy=2&qs=0&rc=M2czO2Q4NHF5eTMzOzczM0ApZjo5OzU5ZWQ3Nzs0Mzo0OGdxY3JvZ18vZnJfLS1jMTZzcy8tLTEyYjRiXzVgYV41Li86Yw%3D%3D&signature=3ddd7ca8bd132b959458993a9e042486&tk=tt_webid_v2&vl=&vr=" >
         </video>
-        <VideoFooter />
-        {/* <VideoSidebar /> */}
+        <VideoFooter 
+          channel='carlyc'
+          description='video of carly tik tok'
+          song='Summertime - Marc X'
+        />
+        <VideoSidebar />
     </div>
   )
 }
--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -3,20 +3,23 @@ import MusicNoteIcon from '@material-ui/icons/MusicNote';
 import Ticker from 'react-ticker'
 import './VideoFooter.css'
 
-function VideoFooter() {
+function VideoFooter({ channel, description, song }) {
+  console.log(song)
   return (
     <div className='videoFooter' >
       <div className='videoFooter__text' >
-        <h3>@carlyclift</h3>
-        <p>this is a video description</p>
-        <MusicNoteIcon />
-        <Ticker mode='smooth'>
-          {({ index }) => (
-            <>
-              <h1>Song goes here</h1>
-            </>
-          )}
-        </Ticker>
+        <h3>@{channel}</h3>
+        <p>{description}</p>
+        <div className='videoFooter__ticker' >
+          <MusicNoteIcon className='videoFooter__icon' />
+          <Ticker mode='smooth'>
+            {({ index }) => (
+                <>
+                <p>{song}</p>
+              </>
+            )}
+          </Ticker>
+        </div>
       </div>
       <img className='videoFooter__record' src='https://static.thenounproject.com/png/934821-200.png' alt='' />
     </div>
